bc16d3efa109ec9a4ce693c39fcad76c4103ad02
objektwerks <objektwerks@runbox.com>
2023-10-05 16:03:13 -0400

    added account to entity

--- shared/src/main/scala/swimmer/Entity.scala
+++ shared/src/main/scala/swimmer/Entity.scala
@@ -1,6 +1,45 @@
 package swimmer
 
+import java.time.LocalDate
+import java.util.UUID
+
+import scala.util.Random
+
 sealed trait Entity:
   val id: Long
+
+final case class Account(id: Long = 0,
+                         license: String = newLicense,
+                         emailAddress: String = "",
+                         pin: String = newPin,
+                         activated: Long = LocalDate.now.toEpochDay,
+                         deactivated: Long = 0) extends Entity:
+  def toArray: Array[Any] = Array(id, license, pin, activated, deactivated)
+
+object Account:
+  private val specialChars = "~!@#$%^&*-+=<>?/:;".toList
+  private val random = new Random
+
+  private def newSpecialChar: Char = specialChars(random.nextInt(specialChars.length))
+
+  private def newPin: String =
+    Random.shuffle(
+      Random
+        .alphanumeric
+        .take(5)
+        .mkString
+        .prepended(newSpecialChar)
+        .appended(newSpecialChar)
+    ).mkString
+
+  private def newLicense: String = UUID.randomUUID.toString
+
+  val empty = Account(
+    license = "",
+    emailAddress = "",
+    pin = "",
+    activated = 0,
+    deactivated = 0
+  )
 
   final case class Swimmer(id: Long = 0) extends Entity
