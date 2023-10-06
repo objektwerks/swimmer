d01c56a71878357d065af5f57e7e75f0f2c0699f
objektwerks <objektwerks@runbox.com>
2023-10-06 08:49:32 -0400

    enhanced serializder

--- shared/src/main/scala/swimmer/Serializer.scala
+++ shared/src/main/scala/swimmer/Serializer.scala
@@ -25,4 +25,6 @@ object Serializer:
   given JsonValueCodec[LoggedIn] = JsonCodecMaker.make[LoggedIn]( CodecMakerConfig.withDiscriminatorFieldName(None) )
   given JsonValueCodec[Deactivated] = JsonCodecMaker.make[Deactivated]( CodecMakerConfig.withDiscriminatorFieldName(None) )
   given JsonValueCodec[Reactivated] = JsonCodecMaker.make[Reactivated]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[SwimmersListed] = JsonCodecMaker.make[SwimmersListed]( CodecMakerConfig.withDiscriminatorFieldName(None) )
+  given JsonValueCodec[SwimmerSaved] = JsonCodecMaker.make[SwimmerSaved]( CodecMakerConfig.withDiscriminatorFieldName(None) )
   given JsonValueCodec[Fault] = JsonCodecMaker.make[Fault]( CodecMakerConfig.withDiscriminatorFieldName(None) )
