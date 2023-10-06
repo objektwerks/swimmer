bcbe82e09a335cbaf29a87c57068af0558d9f201
objektwerks <objektwerks@runbox.com>
2023-10-06 19:27:52 -0400

    refactored swimmer

--- shared/src/main/scala/swimmer/Entity.scala
+++ shared/src/main/scala/swimmer/Entity.scala
@@ -41,8 +41,8 @@ object Account:
   )
 
 final case class Swimmer(id: Long = 0,
-                         license: String = "",
-                         name: String = "") extends Entity
+                         license: String,
+                         name: String) extends Entity
 
 final case class Session(id: Long = 0,
                          swimmerId: Long,
