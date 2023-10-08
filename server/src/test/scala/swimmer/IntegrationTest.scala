af4888682ff8a1e68f93f6c09aee7d8d9cf12171
objektwerks <objektwerks@runbox.com>
2023-10-08 11:11:12 -0400

    added lap to session

--- server/src/test/scala/swimmer/IntegrationTest.scala
+++ server/src/test/scala/swimmer/IntegrationTest.scala
@@ -22,7 +22,7 @@ final class IntegrationTest extends AnyFunSuite with Matchers:
 
   var testAccount = Account()
   var testSwimmer = Swimmer(license = testAccount.license, name = "Fred")
-  var testSession = Session(swimmerId = 0)
+  var testSession = Session(swimmerId = 0, weight = 150, weightUnit = )
 
   test("integration"):
     register
