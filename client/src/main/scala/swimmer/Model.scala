4f9ff6941997eb916b2e4f2786dd96a19521f559
objektwerks <objektwerks@runbox.com>
2023-10-10 11:27:44 -0400

    refactoring model

--- client/src/main/scala/swimmer/Model.scala
+++ client/src/main/scala/swimmer/Model.scala
@@ -18,45 +18,30 @@ final class Model(fetcher: Fetcher) extends LazyLogging:
   val registered = ObjectProperty[Boolean](true)
   val loggedin = ObjectProperty[Boolean](true)
 
-  val selectedPoolId = ObjectProperty[Long](0)
-  val selectedCleaningId = ObjectProperty[Long](0)
-  val selectedMeasurementId = ObjectProperty[Long](0)
+  val selectedSwimmerId = ObjectProperty[Long](0)
+  val selectedSessionId = ObjectProperty[Long](0)
 
-  selectedPoolId.onChange { (_, oldPoolId, newPoolId) =>
-    logger.info(s"*** selected pool id onchange event: $oldPoolId -> $newPoolId")
-    shouldBeInFxThread("*** selected pool id onchange should be in fx thread.")
-    cleanings(newPoolId)
-    measurements(newPoolId)
-    chemicals(newPoolId)
+  selectedSwimmerId.onChange { (_, oldSwimmerId, newSwimmerId) =>
+    logger.info(s"*** selected swimmer id onchange event: $oldSwimmerlId -> $newSwimmerId")
+    shouldBeInFxThread("*** selected swimmer id onchange should be in fx thread.")
+    cleanings(newSwimmerId)
   }
 
   val objectAccount = ObjectProperty[Account](Account.empty)
-  val observablePools = ObservableBuffer[Pool]()
-  val observableCleanings = ObservableBuffer[Cleaning]()
-  val observableMeasurements = ObservableBuffer[Measurement]()
-  val observableChemicals = ObservableBuffer[Chemical]()
+  val observableSwimmers = ObservableBuffer[Swimmer]()
+  val observableSessions = ObservableBuffer[Session]()
   val observableFaults = ObservableBuffer[Fault]()
 
   objectAccount.onChange { (_, oldAccount, newAccount) =>
     logger.info(s"*** object account onchange event: $oldAccount -> $newAccount")
   }
 
-  observablePools.onChange { (_, changes) =>
+  observableSwimmers.onChange { (_, changes) =>
     logger.info(s"*** observable pools onchange event: $changes")
   }
 
-  observableCleanings.onChange { (_, changes) =>
+  observableSessions.onChange { (_, changes) =>
     logger.info(s"*** observable cleanings onchange event: $changes")
-  }
-
-  observableMeasurements.onChange { (_, _) =>
-    logger.info(s"*** observable measurements onchange event.")
-    shouldNotBeInFxThread("***observable measurements onchange should not be in fx thread.")
-    Platform.runLater( dashboard() )
-  }
-
-  observableChemicals.onChange { (_, changes) =>
-    logger.info(s"*** observable chemicals onchange event: $changes")
   }
 
   def onFetchAsyncFault(source: String, fault: Fault): Unit =
@@ -118,37 +103,37 @@ final class Model(fetcher: Fetcher) extends LazyLogging:
         case _ => ()
     )
 
-  def pools(): Unit =
+  def swimmers(): Unit =
     fetcher.fetchAsync(
-      ListPools(objectAccount.get.license),
+      ListSwimmers(objectAccount.get.license),
       (event: Event) => event match
-        case fault @ Fault(_, _) => onFetchAsyncFault("Model.pools", fault)
-        case PoolsListed(pools) =>
-          observablePools.clear()
-          observablePools ++= pools
+        case fault @ Fault(_, _) => onFetchAsyncFault("Model.swimmers", fault)
+        case SwimmersListed(swimmers) =>
+          observableSwimmers.clear()
+          observableSwimmers ++= swimmers
         case _ => ()
     )
 
-  def add(selectedIndex: Int, pool: Pool)(runLast: => Unit): Unit =
+  def add(selectedIndex: Int, swimmer: Swimmer)(runLast: => Unit): Unit =
     fetcher.fetchAsync(
-      SavePool(objectAccount.get.license, pool),
+      SaveSwimmer(objectAccount.get.license, swimmer),
       (event: Event) => event match
-        case fault @ Fault(_, _) => onFetchAsyncFault("Model.save pool", pool, fault)
-        case PoolSaved(id) =>
-          observablePools += pool.copy(id = id)
-          observablePools.sort()
-          selectedPoolId.set(id)
+        case fault @ Fault(_, _) => onFetchAsyncFault("Model.save swimmer", swimmer, fault)
+        case SwimmerSaved(id) =>
+          observableSwimmers += swimmer.copy(id = id)
+          observableSwimmers.sort()
+          selectedSwimmerId.set(id)
           runLast
         case _ => ()
     )
 
-  def update(selectedIndex: Int, pool: Pool)(runLast: => Unit): Unit =
+  def update(selectedIndex: Int, swimmer: Swimmer)(runLast: => Unit): Unit =
     fetcher.fetchAsync(
-      SavePool(objectAccount.get.license, pool),
+      SaveSwimmer(objectAccount.get.license, swimmer),
       (event: Event) => event match
-        case fault @ Fault(_, _) => onFetchAsyncFault("Model.save pool", pool, fault)
-        case PoolSaved(id) =>
-          observablePools.update(selectedIndex, pool)
+        case fault @ Fault(_, _) => onFetchAsyncFault("Model.save swimmer", swimmer, fault)
+        case SwimmerSaved(id) =>
+          observableSwimmers.update(selectedIndex, swimmer)
           runLast
         case _ => ()
     )
@@ -159,8 +144,8 @@ final class Model(fetcher: Fetcher) extends LazyLogging:
       (event: Event) => event match
         case fault @ Fault(_, _) => onFetchAsyncFault("Model.cleanings", fault)
         case CleaningsListed(cleanings) =>
-          observableCleanings.clear()
-          observableCleanings ++= cleanings
+          observableSessions.clear()
+          observableSessions ++= cleanings
         case _ => ()
     )
 
@@ -170,9 +155,9 @@ final class Model(fetcher: Fetcher) extends LazyLogging:
       (event: Event) => event match
         case fault @ Fault(_, _) => onFetchAsyncFault("Model.save cleaning", cleaning, fault)
         case CleaningSaved(id) =>
-          observableCleanings += cleaning.copy(id = id)
-          observableCleanings.sort()
-          selectedCleaningId.set(id)
+          observableSessions += cleaning.copy(id = id)
+          observableSessions.sort()
+          selectedSessionId.set(id)
           runLast
         case _ => ()
     )
@@ -183,153 +168,7 @@ final class Model(fetcher: Fetcher) extends LazyLogging:
       (event: Event) => event match
         case fault @ Fault(_, _) => onFetchAsyncFault("Model.save cleaning", cleaning, fault)
         case CleaningSaved(id) =>
-          observableCleanings.update(selectedIndex, cleaning)
+          observableSessions.update(selectedIndex, cleaning)
           runLast
         case _ => ()
     )
-
-  def measurements(poolId: Long): Unit =
-    fetcher.fetchAsync(
-      ListMeasurements(objectAccount.get.license, poolId),
-      (event: Event) => event match
-        case fault @ Fault(_, _) => onFetchAsyncFault("Model.measurements", fault)
-        case MeasurementsListed(measurements) =>
-          observableMeasurements.clear()
-          observableMeasurements ++= measurements
-        case _ => ()
-    )
-
-  def add(selectedIndex: Int, measurement: Measurement)(runLast: => Unit): Unit =
-    fetcher.fetchAsync(
-      SaveMeasurement(objectAccount.get.license, measurement),
-      (event: Event) => event match
-        case fault @ Fault(_, _) => onFetchAsyncFault("Model.save measurement", measurement, fault)
-        case MeasurementSaved(id) =>
-          observableMeasurements += measurement.copy(id = id)
-          observableMeasurements.sort()
-          selectedMeasurementId.set(id)
-          runLast
-        case _ => ()
-    )
-
-  def update(selectedIndex: Int, measurement: Measurement)(runLast: => Unit): Unit =
-    fetcher.fetchAsync(
-      SaveMeasurement(objectAccount.get.license, measurement),
-      (event: Event) => event match
-        case fault @ Fault(_, _) => onFetchAsyncFault("Model.save measurement", measurement, fault)
-        case MeasurementSaved(id) =>
-          observableMeasurements.update(selectedIndex, measurement)
-          runLast
-        case _ => ()
-    )
-
-  def chemicals(poolId: Long): Unit =
-    fetcher.fetchAsync(
-      ListChemicals(objectAccount.get.license, poolId),
-      (event: Event) => event match
-        case fault @ Fault(_, _) => onFetchAsyncFault("Model.chemicals", fault)
-        case ChemicalsListed(chemicals) =>
-          observableChemicals.clear()
-          observableChemicals ++= chemicals
-        case _ => ()
-    )
-  
-  def add(selectedIndex: Int, chemical: Chemical)(runLast: => Unit): Unit =
-    fetcher.fetchAsync(
-      SaveChemical(objectAccount.get.license, chemical),
-      (event: Event) => event match
-        case fault @ Fault(_, _) => onFetchAsyncFault("Model.save chemical", chemical, fault)
-        case ChemicalSaved(id) =>
-          observableChemicals += chemical.copy(id = id)
-          observableChemicals.sort()
-          selectedChemicalId.set(id)
-          runLast
-        case _ => ()
-    )
-
-  def update(selectedIndex: Int, chemical: Chemical)(runLast: => Unit): Unit =
-    fetcher.fetchAsync(
-      SaveChemical(objectAccount.get.license, chemical),
-      (event: Event) => event match
-        case fault @ Fault(_, _) => onFetchAsyncFault("Model.save chemical", chemical, fault)
-        case ChemicalSaved(id) =>
-          observableChemicals.update(selectedIndex, chemical)
-          runLast
-        case _ => ()
-    )
-
-  val currentTotalChlorine = ObjectProperty[Int](0)
-  val averageTotalChlorine = ObjectProperty[Int](0)
-  def totalChlorineInRange(value: Int): Boolean = totalChlorineRange.contains(value)
-
-  val currentFreeChlorine = ObjectProperty[Int](0)
-  val averageFreeChlorine = ObjectProperty[Int](0)
-  def freeChlorineInRange(value: Int): Boolean = freeChlorineRange.contains(value)
-
-  val currentCombinedChlorine = ObjectProperty[Double](0)
-  val averageCombinedChlorine = ObjectProperty[Double](0)
-  def combinedChlorineInRange(value: Double): Boolean = combinedChlorineRange.contains(value)
-
-  val currentPh = ObjectProperty[Double](0)
-  val averagePh = ObjectProperty[Double](0)
-  def phInRange(value: Double): Boolean = phRange.contains(value)
-
-  val currentCalciumHardness = ObjectProperty[Int](0)
-  val averageCalciumHardness = ObjectProperty[Int](0)
-  def calciumHardnessInRange(value: Int): Boolean = calciumHardnessRange.contains(value)
-
-  val currentTotalAlkalinity = ObjectProperty[Int](0)
-  val averageTotalAlkalinity = ObjectProperty[Int](0)
-  def totalAlkalinityInRange(value: Int): Boolean = totalAlkalinityRange.contains(value)
-
-  val currentCyanuricAcid = ObjectProperty[Int](0)
-  val averageCyanuricAcid = ObjectProperty[Int](0)
-  def cyanuricAcidInRange(value: Int): Boolean = cyanuricAcidRange.contains(value)
-
-  val currentTotalBromine = ObjectProperty[Int](0)
-  val averageTotalBromine = ObjectProperty[Int](0)
-  def totalBromineInRange(value: Int): Boolean = totalBromineRange.contains(value)
-
-  val currentSalt = ObjectProperty[Int](0)
-  val averageSalt = ObjectProperty[Int](0)
-  def saltInRange(value: Int): Boolean = saltRange.contains(value)
-
-  val currentTemperature = ObjectProperty[Int](0)
-  val averageTemperature = ObjectProperty[Int](0)
-  def temperatureInRange(value: Int): Boolean = temperatureRange.contains(value)
-
-  private def dashboard(): Unit =
-    shouldBeInFxThread("*** dashboard should be in fx thread.")
-    val numberFormat = NumberFormat.getNumberInstance()
-    numberFormat.setMaximumFractionDigits(1)
-    observableMeasurements.headOption.foreach { measurement =>
-      calculateCurrentMeasurements(measurement, numberFormat)
-      calculateAverageMeasurements(numberFormat)
-    }
-
-  private def calculateCurrentMeasurements(measurement: Measurement, numberFormat: NumberFormat): Unit =
-    shouldBeInFxThread("*** calculateCurrentMeasurements should be in fx thread.")
-    currentTotalChlorine.value = measurement.totalChlorine
-    currentFreeChlorine.value = measurement.freeChlorine
-    currentCombinedChlorine.value = numberFormat.format( measurement.combinedChlorine ).toDouble
-    currentPh.value = numberFormat.format( measurement.ph ).toDouble
-    currentCalciumHardness.value = measurement.calciumHardness
-    currentTotalAlkalinity.value = measurement.totalAlkalinity
-    currentCyanuricAcid.value = measurement.cyanuricAcid
-    currentTotalBromine.value = measurement.totalBromine
-    currentSalt.value = measurement.salt
-    currentTemperature.value = measurement.temperature
-
-  private def calculateAverageMeasurements(numberFormat: NumberFormat): Unit =
-    shouldBeInFxThread("*** calculateAverageMeasurements should be in fx thread.")
-    val count = observableMeasurements.length
-    averageTotalChlorine.value = observableMeasurements.map(_.totalChlorine).sum / count
-    averageFreeChlorine.value = observableMeasurements.map(_.freeChlorine).sum / count
-    averageCombinedChlorine.value = numberFormat.format( observableMeasurements.map(_.combinedChlorine).sum / count ).toDouble
-    averagePh.value = numberFormat.format( observableMeasurements.map(_.ph).sum / count ).toDouble
-    averageCalciumHardness.value = observableMeasurements.map(_.calciumHardness).sum / count
-    averageTotalAlkalinity.value = observableMeasurements.map(_.totalAlkalinity).sum / count
-    averageCyanuricAcid.value = observableMeasurements.map(_.cyanuricAcid).sum / count
-    averageTotalBromine.value = observableMeasurements.map(_.totalBromine).sum / count
-    averageSalt.value = observableMeasurements.map(_.salt).sum / count
-    averageTemperature.value = observableMeasurements.map(_.temperature).sum / count
