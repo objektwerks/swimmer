5c410ff4805f3e8d92c79e9f161c71c2c27d36cf
objektwerks <objektwerks@runbox.com>
2023-10-12 06:04:16 -0400

    added sessiong dialog and int text field

---
+++ client/src/main/scala/swimmer/dialog/SessionDialog.scala
@@ -0,0 +1,46 @@
+package swimmer.dialog
+
+import scalafx.Includes.*
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.layout.Region
+import scalafx.scene.control.{ButtonType, ComboBox, Dialog, TextField}
+import scalafx.scene.control.ButtonBar.ButtonData
+
+import swimmer.{Client, Context, Session}
+import swimmer.control.IntTextField
+
+final class PoolDialog(context: Context, session: Session) extends Dialog[Session]:
+  initOwner(Client.stage)
+  title = context.windowTitle
+  headerText = context.dialogSession
+
+  val nameTextField = new TextField:
+    text = pool.name
+
+  val volumeTextField = new IntTextField:
+    text = pool.volume.toString
+  
+  val unitComboBox = new ComboBox[String]:
+  	items = ObservableBuffer.from( UnitOfMeasure.toPoolList )
+  	value = pool.unit.toString
+  unitComboBox.prefWidth = 200
+
+  val controls = List[(String, Region)](
+    context.labelName -> nameTextField,
+    context.labelVolume -> volumeTextField,
+    context.labelUnit -> unitComboBox
+  )
+  dialogPane().content = ControlGridPane(controls)
+
+  val saveButtonType = new ButtonType(context.buttonSave, ButtonData.OKDone)
+  dialogPane().buttonTypes = List(saveButtonType, ButtonType.Cancel)
+
+  resultConverter = dialogButton => {
+    if dialogButton == saveButtonType then
+      pool.copy(
+        name = nameTextField.text.value,
+        volume = volumeTextField.text.value.toIntOption.getOrElse(pool.volume),
+        unit = unitComboBox.value.value
+      )
+    else null
+  }
