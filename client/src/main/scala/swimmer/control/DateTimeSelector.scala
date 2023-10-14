4e0d6078205dddd6f7f90c62b10114b5c630b011
objektwerks <objektwerks@runbox.com>
2023-10-14 08:38:48 -0400

    enhanced date time selector

--- client/src/main/scala/swimmer/control/DateTimeSelector.scala
+++ client/src/main/scala/swimmer/control/DateTimeSelector.scala
@@ -10,18 +10,34 @@ final class DateTimeSelector(localDateTime: LocalDateTime) extends HBox:
   spacing = 3
   padding = Insets(3)
 
+  val labelYear = new Label:
+    alignment = Pos.CENTER_LEFT
+    text = "Year:"
+
+  val labelMonth = new Label:
+    alignment = Pos.CENTER_LEFT
+    text = "Month:"
+
+  val labelDay = new Label:
+    alignment = Pos.CENTER_LEFT
+    text = "Day:"
+
   val labelHour = new Label:
     alignment = Pos.CENTER_LEFT
-    text = "H:"
+    text = "Hour:"
 
-  val labelMinute = new Label:
+  val labelMinutes = new Label:
     alignment = Pos.CENTER_LEFT
-    text = "M:"
+    text = "Minutes:"
+
+  val labelSecond = new Label:
+    alignment = Pos.CENTER_LEFT
+    text = "Seconds:"
   
   val hourSpinner = Spinner[Int](min = 0, max = 23, initialValue = localDateTime.getHour, amountToStepBy = 1)
   val minuteSpinner = Spinner[Int](min = 0, max = 59, initialValue = localDateTime.getMinute, amountToStepBy = 1)
 
-  children = List(labelHour, hourSpinner, labelMinute, minuteSpinner)
+  children = List(labelHour, hourSpinner, labelMinutes, minuteSpinner)
   HBox.setHgrow(this, Priority.Always)
 
   def value = localDateTime.withHour( hourSpinner.value.value ).withMinute( minuteSpinner.value.value )
