ba8dbab44ed35c0752d47ffdb657e4f3baaf5c5a
objektwerks <objektwerks@runbox.com>
2023-10-14 09:02:45 -0400

    enhanced date time selector

--- client/src/main/scala/swimmer/control/DateTimeSelector.scala
+++ client/src/main/scala/swimmer/control/DateTimeSelector.scala
@@ -4,7 +4,7 @@ import java.time.LocalDateTime
 
 import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.control.{Label, Spinner}
-import scalafx.scene.layout.VBox
+import scalafx.scene.layout.{GridPane, VBox}
 
 final class DateTimeSelector(localDateTime: LocalDateTime) extends VBox:
   spacing = 3
@@ -41,4 +41,25 @@ final class DateTimeSelector(localDateTime: LocalDateTime) extends VBox:
   val minuteSpinner = Spinner[Int](min = 0, max = 59, initialValue = localDateTime.getMinute, amountToStepBy = 1)
   val secondSpinner = Spinner[Int](min = 0, max = 59, initialValue = localDateTime.getSecond, amountToStepBy = 1)
 
+  val controls = List[(Label, Spinner[Int])](
+    labelYear -> yearSpinner
+
+  )
+
+  val selector = toGridPane(controls)
+
   children = List()
+
+  private def toGridPane(controls: List[(Label, Spinner[Int])]): GridPane =
+    val gridPane = new GridPane:
+      hgap = 6
+      vgap = 6
+      padding = Insets(top = 6, right = 6, bottom = 6, left = 6)
+    
+    var row = 0
+    for ((label, spinner) <- controls)
+      gridPane.add(label, columnIndex = 0, rowIndex = row)
+      gridPane.add(spinner, columnIndex = 1, rowIndex = row)
+      row += 1
+
+    gridPane
