a33351540c780ebf6bb17367da3ca5e6f68b5079
objektwerks <objektwerks@runbox.com>
2023-10-16 16:53:42 -0400

    edited popup in date time selector

--- client/src/main/scala/swimmer/control/DateTimeSelector.scala
+++ client/src/main/scala/swimmer/control/DateTimeSelector.scala
@@ -30,8 +30,8 @@ final class DateTimeSelector(context: Context, localDateTime: LocalDateTime) ext
 
   private def showPopup(): Unit =
     val popup = Popup()
-    popup.setAutoHide(true)
-    popup.setHideOnEscape(true)
+    popup.setAutoHide(false)
+    popup.setHideOnEscape(false)
     val popupView = PopupView(context, value.value, popup, popupValue)
     popup.content.addOne(popupView)
     popup.show(Client.stage)
