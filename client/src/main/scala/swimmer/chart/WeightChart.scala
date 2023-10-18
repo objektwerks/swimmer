570de20e07a203fba3d6e2a07cc1387771fbcddb
objektwerks <objektwerks@runbox.com>
2023-10-18 13:37:41 -0400

    added weight chart

---
+++ client/src/main/scala/swimmer/chart/WeightChart.scala
@@ -0,0 +1,44 @@
+package swimmer.chart
+
+import java.time.format.DateTimeFormatter
+
+import scalafx.Includes.*
+import scalafx.geometry.Insets
+import scalafx.scene.chart.{LineChart, XYChart}
+import scalafx.scene.control.{Tab, TabPane}
+
+import swimmer.{Context, Entity, Model}
+
+final case class WeightXY(xDate: String, yCount: Int)
+
+final class WeightChart(context: Context, model: Model) extends TabPane:
+  val expendables = model.observableSessions.reverse
+  val dateFormat = DateTimeFormatter.ofPattern("M.dd")
+  val minDate = Entity.toLocalDateTime( expendables.map(e => e.datetime).min ).format(dateFormat)
+  val maxDate = Entity.toLocalDateTime( expendables.map(e => e.datetime).max ).format(dateFormat)
+
+  val tab = new Tab:
+    closable = false
+    text = context.tabCalories
+    content = buildChart()
+
+  padding = Insets(6)
+  tabs = List(tab)
+
+  def buildChart(): LineChart[String, Number] =
+    val filtered = expendables.map(e => CalorieXY( Entity.toLocalDateTime(e.datetime).format(dateFormat), e.weight) )
+    val (chart, series) = LineChartBuilder.build(context = context,
+                                                 xLabel = context.chartMonthDay,
+                                                 xMinDate = minDate,
+                                                 xMaxDate = maxDate,
+                                                 yLabel = context.headerCalories,
+                                                 yLowerBound = 50,
+                                                 yUpperBound = 400,
+                                                 yTickUnit = 50,
+                                                 yValues = filtered.map(exy => exy.yCount))
+    filtered foreach { exy =>
+      series.data() += XYChart.Data[String, Number](exy.xDate.format(dateFormat), exy.yCount)
+    }
+    chart.data = series
+    LineChartBuilder.addTooltip(chart)
+    chart
