ce46fb05cf7ca7964547ef5ec3872355bd7f1eba
objektwerks <objektwerks@runbox.com>
2023-10-19 14:35:00 -0400

    edited weight chart

--- client/src/main/scala/swimmer/chart/WeightChart.scala
+++ client/src/main/scala/swimmer/chart/WeightChart.scala
@@ -11,17 +11,17 @@ import swimmer.{Context, Entity, Model}
 final case class WeightXY(xDate: String, yCount: Int)
 
 final class WeightChart(context: Context, model: Model) extends Tab:
-  val weights = model.observableSessions.reverse
+  val sessions = model.observableSessions.reverse
   val dateFormat = DateTimeFormatter.ofPattern("M.dd")
-  val minDate = Entity.toLocalDateTime( weights.map(e => e.datetime).min ).format(dateFormat)
-  val maxDate = Entity.toLocalDateTime( weights.map(e => e.datetime).max ).format(dateFormat)
+  val minDate = Entity.toLocalDateTime( sessions.map(e => e.datetime).min ).format(dateFormat)
+  val maxDate = Entity.toLocalDateTime( sessions.map(e => e.datetime).max ).format(dateFormat)
 
   closable = false
   text = context.tabWeight
   content = buildChart()
 
   def buildChart(): LineChart[String, Number] =
-    val filtered = weights.map(e => WeightXY( Entity.toLocalDateTime(e.datetime).format(dateFormat), e.weight) )
+    val filtered = sessions.map(s => WeightXY( Entity.toLocalDateTime(s.datetime).format(dateFormat), s.weight) )
     val (chart, series) = LineChartBuilder.build(context = context,
                                                  xLabel = context.chartMonthDay,
                                                  xMinDate = minDate,
