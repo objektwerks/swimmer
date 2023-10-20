1aea8e77e8bec8a92dd762930b3505f90f502609
objektwerks <objektwerks@runbox.com>
2023-10-20 05:01:08 -0400

    edited weight unit test

--- shared/src/test/scala/swimmer/WeightUnitTest.scala
+++ shared/src/test/scala/swimmer/WeightUnitTest.scala
@@ -7,9 +7,9 @@ import WeightUnit.*
 
 final class WeightUnitTest extends AnyFunSuite with Matchers:
   test("lbs to kgs"):
-    lbsToKgs(1.0) shouldBe 0.454
-    lbsToKgs(2.0) shouldBe 0.908
+    lbsToKgs(1) shouldBe 0.454
+    lbsToKgs(2) shouldBe 0.908
 
   test("kgs to lbs"):
-    kgsToLbs(1.0) shouldBe 2.205
-    kgsToLbs(2.0) shouldBe 4.41
+    kgsToLbs(1) shouldBe 2.205
+    kgsToLbs(2) shouldBe 4.41
