0207a4e7ab5f5de86cc046e430bd21b5a8cd8342
objektwerks <objektwerks@runbox.com>
2023-10-21 12:54:31 -0400

    enhanced chart dialog

--- client/src/main/scala/swimmer/dialog/ChartDialog.scala
+++ client/src/main/scala/swimmer/dialog/ChartDialog.scala
@@ -2,10 +2,14 @@ package swimmer.dialog
 
 import scalafx.scene.control.{Dialog, TabPane}
 
-import swimmer.{Context, Model}
+import swimmer.{Client, Context, Model}
 import swimmer.chart.{CaloriesChart, DistanceChart, WeightChart}
 
 class ChartDialog(context: Context, model: Model) extends Dialog:
+  initOwner(Client.stage)
+  title = context.windowTitle
+  headerText = context.dialogChart
+
   val tabPane = new TabPane:
     tabs = List(
       DistanceChart(context, model),
