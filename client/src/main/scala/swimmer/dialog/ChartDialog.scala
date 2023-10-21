643bb404fe5f8638bb75810ac259dddf143168a1
objektwerks <objektwerks@runbox.com>
2023-10-21 12:59:04 -0400

    enhanced chart dialog

--- client/src/main/scala/swimmer/dialog/ChartDialog.scala
+++ client/src/main/scala/swimmer/dialog/ChartDialog.scala
@@ -1,18 +1,25 @@
 package swimmer.dialog
 
-import scalafx.scene.control.{Dialog, TabPane}
+import scalafx.Includes.*
+import scalafx.scene.control.{ButtonType, Dialog, TabPane}
+import scalafx.scene.layout.VBox
 
 import swimmer.{Client, Context, Model}
 import swimmer.chart.{CaloriesChart, DistanceChart, WeightChart}
 
-class ChartDialog(context: Context, model: Model) extends Dialog:
+final class ChartDialog(context: Context, model: Model) extends Dialog:
   initOwner(Client.stage)
   title = context.windowTitle
   headerText = context.dialogCharts
 
-  val tabPane = new TabPane:
+  val chartsTabPane = new TabPane:
     tabs = List(
       DistanceChart(context, model),
       CaloriesChart(context, model),
       WeightChart(context, model)
     )
+
+  dialogPane().buttonTypes = List(ButtonType.Close)
+  dialogPane().content = new VBox:
+    spacing = 6
+    children = List(chartsTabPane)
