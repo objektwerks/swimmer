c62606c18dfe1fcde79fa2dff87f15ca5e312378
objektwerks <objektwerks@runbox.com>
2024-07-24 05:59:48 -0400

    refactored dispatcher dispatch

--- server/src/main/scala/swimmer/Dispatcher.scala
+++ server/src/main/scala/swimmer/Dispatcher.scala
@@ -9,29 +9,25 @@ sealed trait Security
 case object Authorized extends Security
 final case class Unauthorized(cause: String) extends Security
 
-final class Dispatcher(store: Store, emailer: Emailer):
-  def dispatch[E <: Event](command: Command): Event =
-    if !command.isValid then store.addFault( Fault(s"Command is invalid: $command") )
-    
-    isAuthorized(command) match
-      case Authorized(isAuthorized) => if !isAuthorized then store.addFault( Fault(s"License is unauthorized: $command") )
-      case fault @ Fault(_, _) => store.addFault(fault)
-      case _ =>
-        
-    val event = command match
-      case Register(emailAddress)     => register(emailAddress)
-      case Login(emailAddress, pin)   => login(emailAddress, pin)
-      case Deactivate(license)        => deactivateAccount(license)
-      case Reactivate(license)        => reactivateAccount(license)
-      case ListSwimmers(_, accountId) => listSwimmers(accountId)
-      case SaveSwimmer(_, swimmer)    => saveSwimmer(swimmer)
-      case ListSessions(_, swimmerId) => listSessions(swimmerId)
-      case SaveSession(_, session)    => saveSession(session)
-      case AddFault(_, fault)         => addFault(fault)
-
-    event match
-      case fault @ Fault(_, _) => store.addFault(fault)
-      case _ => event
+final class Dispatcher(store: Store,
+                       emailer: Emailer):
+  def dispatch(command: Command): Event =
+    command.isValid match
+      case false => Fault(s"Invalid command: $command")
+      case true =>
+        isAuthorized(command) match
+          case Unauthorized(cause) => Fault(cause)
+          case Authorized =>
+            command match
+              case Register(emailAddress)     => register(emailAddress)
+              case Login(emailAddress, pin)   => login(emailAddress, pin)
+              case Deactivate(license)        => deactivateAccount(license)
+              case Reactivate(license)        => reactivateAccount(license)
+              case ListSwimmers(_, accountId) => listSwimmers(accountId)
+              case SaveSwimmer(_, swimmer)    => saveSwimmer(swimmer)
+              case ListSessions(_, swimmerId) => listSessions(swimmerId)
+              case SaveSession(_, session)    => saveSession(session)
+              case AddFault(_, fault)         => addFault(fault)
 
   def isAuthorized(command: Command): Security =
     command match
